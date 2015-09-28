b2a48eecf774c7b85f828528b2030af9ac80a496
Hirofumi Hamada <hiro1110arp@gmail.com>
2015-09-28 14:54:03 +0900

    first commit

---
+++ master.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+
+/* define SS Pin */
+#define SS_pin (10)
+
+/* global variable */ 
+char val = 0xF;
+ 
+ 
+/***********************************
+ * setup function
+ **********************************/
+void setup()
+{
+  /* SS_pin mode set */
+  pinMode(SS_pin, OUTPUT);
+
+  /* SPI Master Initialize */ 
+  SPI.begin();
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+}
+
+
+/***********************************
+ * main loop function
+ **********************************/ 
+void loop()
+{
+  digitalWrite(SS_pin, LOW);
+  
+  SPI.transfer(val);
+  digitalWrite(SS_pin, HIGH);  
+ 
+  delay(1000);
+}
---
+++ slave.ino
@@ -0,0 +1,52 @@
+/*=============================================
+ * For Arduino UNO SPI Slave
+ * Receive 1 byte data and 
+ * display it to Serial Monitor via uart
+ *
+ * Hirofumi Hamada
+ * ver.1.00
+ *============================================*/
+
+#include <SPI.h>
+
+/*-----------------------------------
+ * SPI_Slave_Receive
+ *---------------------------------*/
+ char spi_slave_receive(void)
+ {
+   /* Wait for reception complete */
+   while(!(SPSR & (1 << SPIF)))
+     ;
+    
+   /* Return Data Register */
+   return SPDR;
+ }
+ 
+ 
+/***********************************
+ * setup function
+ **********************************/
+void setup()
+{
+  Serial.begin(9600);
+  
+  /* Initialize SPI */
+  pinMode(SS, INPUT);
+  pinMode(SCK, INPUT);
+  pinMode(MOSI, INPUT);  
+  pinMode(MISO, OUTPUT);
+  
+  /* enable SPI */
+  SPCR = (1 << SPE);  // no Interrrupt, MSB first, Slave select 
+}
+ 
+/***********************************
+ * main loop function
+ **********************************/
+void loop()
+{
+  Serial.println("start SPI receive");
+  char data = spi_slave_receive();
+  Serial.print("data = 0x");
+  Serial.println(data, HEX);
+}
